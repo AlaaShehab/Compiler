22c420c1e84ccb12468df96486f7453e66ca9498
akrammousa <akrammousa2014@gmail.com>
2019-03-23 22:12:30 +0200

    GeneratorClasses

---
+++ Lexical_Analyzer/DFAGenerator.cpp
@@ -0,0 +1,9 @@
+//
+// Created by akrammoussa on 22/03/19.
+//
+
+#include "DFAGenerator.h"
+
+vector<Node*>  DFAGenerator::generateDFATable(vector<Node *>) {
+
+}
---
+++ Lexical_Analyzer/DFAGenerator.h
@@ -0,0 +1,18 @@
+//
+// Created by akrammoussa on 22/03/19.
+//
+
+#ifndef LEXICAL_ANALYZER_DFAGENERATOR_H
+#define LEXICAL_ANALYZER_DFAGENERATOR_H
+
+
+#include "Node.h"
+
+class DFAGenerator {
+public:
+    vector<Node*> generateDFATable(vector<Node*>);
+
+};
+
+
+#endif //LEXICAL_ANALYZER_DFAGENERATOR_H
