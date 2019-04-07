78500661e07fed2b3e2f90861611df4d9e51d174
akrammousa <akrammousa2014@gmail.com>
2019-04-08 01:40:50 +0200

    Edit in DFAGenerator

--- Lexical_Analyzer/DFAGenerator.cpp
+++ Lexical_Analyzer/DFAGenerator.cpp
@@ -11,14 +11,29 @@
 using namespace std;
 
 void fillInClosure(DFANode*,vector<Node *>){
-    //todo
+    //todo after fixing the epsilon string
 }
 
 bool checkIfExists(vector<int> nameList , vector<DFANode *> DFA){
-
+    sort(nameList.begin() , nameList.end());
+    for (int i = 0; i < DFA.size(); ++i) {
+        vector<int> tempName = DFA[i]->getNodeNameList();
+        sort(tempName.begin() , tempName.end());
+        if (nameList == tempName){
+            return true;
+        }
+        return false;
+    }
 }
 DFANode* getNodeFromName(vector<int> nodeNameList, vector<DFANode *> DFA){
-    //todo
+    sort(nodeNameList.begin() , nodeNameList.end());
+    for (int i = 0; i < DFA.size(); ++i) {
+        vector<int> tempName = DFA[i]->getNodeNameList();
+        sort(tempName.begin() , tempName.end());
+        if (nodeNameList == tempName){
+            return DFA[i];
+        }
+    }
 }
 void setEndNodes(vector<Node *> NFA,vector<DFANode *> DFA ){
     for (int i = 0; i < NFA.size(); ++i) {
@@ -39,6 +54,22 @@ void setEndNodes(vector<Node *> NFA,vector<DFANode *> DFA ){
 
 vector<int> tranistionTo(char *input, int target, vector<Node *> NFA) {
     //todo: it takes char inout and target from closure and return nextNodes when givin that input
+    Node* tempNode;
+    vector<int> listOfTransitions;
+    for (int i = 0; i < NFA.size(); ++i) {
+        if (NFA[i]->getName() == target) {
+            tempNode = NFA[i];
+            break;
+        }
+    }
+    vector<Transition> tempTransitionList  = tempNode->getTransition();
+    for (int j = 0; j <tempTransitionList.size() ; ++j) {
+        if (tempTransitionList[j].getInput() == input){
+            listOfTransitions.push_back(tempTransitionList[j].getNode()->getName());
+        }
+    }
+    return listOfTransitions;
+
 }
 void DFAGenerator::initializeClosureLists(vector<Node *> NFA) {
 
@@ -66,7 +97,9 @@ void DFAGenerator::initializeClosureLists(vector<Node *> NFA) {
 vector<Node*>  DFAGenerator::generateDFATable(vector<Node *> NFA , vector<char*> inputs,Node* start) {
     vector<DFANode *> DFA ;
     queue<DFANode *> buildingQueue;
-    DFANode* startNode = new DFANode();
+    int nameCounter = 0;
+    DFANode* startNode = new DFANode(nameCounter);
+    nameCounter++;
     startNode->addName(start->getName());
     startNode->setStartNode(true);
     fillInClosure(startNode,NFA);
@@ -78,7 +111,8 @@ vector<Node*>  DFAGenerator::generateDFATable(vector<Node *> NFA , vector<char*>
         buildingQueue.pop();
         vector<int> tempClosure = tempNode->getEpslonClosure();
         for (int i = 0; i < inputs.size() ; ++i) {
-            DFANode* newNode = new DFANode();
+            DFANode* newNode = new DFANode(nameCounter);
+            nameCounter++;
             for (int j = 0; j < tempClosure.size(); ++j) {
                 vector<int> tempVector = tranistionTo(inputs[i] ,tempClosure[j] , NFA );
                 for (int k = 0; k <tempVector.size() ; ++k) {
@@ -90,6 +124,7 @@ vector<Node*>  DFAGenerator::generateDFATable(vector<Node *> NFA , vector<char*>
                 buildingQueue.push(newNode);
                 tempNode->addTransition(Transition(newNode, inputs[i]));
             } else{
+                nameCounter --;
                 newNode = getNodeFromName(newNode->getNodeNameList(),DFA);
                 tempNode->addTransition(Transition(newNode, inputs[i]));
             }
